552670b121d4653449b95e29f4f8c810fcb00e40
Jordi Chorro Gimeno <100350641+Giorgio02K@users.noreply.github.com>
2024-02-06 19:51:11 +0100

    Delete proyecto_nomadas directory

--- proyecto_nomadas/src/main/java/org/example/proyecto_nomadas/repository/IImagenRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.proyecto_nomadas.repository;
-
-import org.example.proyecto_nomadas.model.Imagen;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IImagenRepository extends JpaRepository<Imagen, Long> {
-}
