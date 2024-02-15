5c059c1772ab27d29159ed5a150663a744f9f8a2
Angel17ms <morenosuarezangel@gmail.com>
2024-02-15 17:28:46 +0100

    commit

--- proyecto_nomadas/src/main/java/org/example/proyecto_nomadas/controller/OdooController.java
+++ proyecto_nomadas/src/main/java/org/example/proyecto_nomadas/controller/OdooController.java
@@ -2,6 +2,7 @@ package org.example.proyecto_nomadas.controller;
 
 import org.example.proyecto_nomadas.service.OdooService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +22,15 @@ public class OdooController {
         String username = "Juan";
         String password = "1234";
         return odooService.loginEnOdoo(url, db, username, password);
+    }
+
+    @PostMapping("/venta/{id_user}")
+    public Object crearVenta(@PathVariable("id_user") int id_user) {
+        String url = "http://192.168.8.110:8069/jsonrpc";
+        String db = "Proyecto";
+        String username = "Juan";
+        String password = "1234";
+        return odooService.crearVenta(url, db,2, "admin", id_user);
     }
 
 }
--- proyecto_nomadas/src/main/java/org/example/proyecto_nomadas/service/OdooService.java
+++ proyecto_nomadas/src/main/java/org/example/proyecto_nomadas/service/OdooService.java
@@ -9,7 +9,9 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
 import java.util.Map;
-
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 @Service
@@ -35,6 +37,122 @@ public class OdooService {
         HttpEntity<JsonRpcRequest> entity = new HttpEntity<>(request, headers);
 
         return restTemplate.postForObject(url, entity, Object.class);
+    }
+
+    public Object crearVenta(String url, String db, int adminId, String adminPassword, int idUser) {
+        //Busca si existe el producto y guarda su id
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        Map<String, Object> argsExisteProducto = new HashMap<>();
+        argsExisteProducto.put("service", "object");
+        argsExisteProducto.put("method", "execute");
+        argsExisteProducto.put("args", new Object[]{db, adminId,adminPassword,"product.product", "search_read",new Object[]{},new String[]{"name"}});
+
+        JsonRpcRequest requestExisteProducto = new JsonRpcRequest("call", argsExisteProducto, 2);
+        HttpEntity<JsonRpcRequest> entityExisteProducto = new HttpEntity<>(requestExisteProducto);
+
+        String jsonResponse = Objects.requireNonNull(restTemplate.postForObject(url, entityExisteProducto, Object.class)).toString();
+        int productId = -1;
+        String productoAComprar = "Suscripción Premium";
+
+        //Los Pattern son simplemente para que busque dentro del Json que devuelve Odoo para no hacer algo mas complicado para sacar la informacion
+        Pattern productoPattern = Pattern.compile("id=(\\d+), name=([^,}]+)");
+        Matcher matcher = productoPattern.matcher(jsonResponse);
+
+        //Encuentra todas las coincidencias y almacena los valores en variables
+        while (matcher.find()) {
+            String id = matcher.group(1);
+            String name = matcher.group(2);
+            //Si encuentra la Subscripcion Premium guarda el id para generar la venta
+            if (name.equalsIgnoreCase(productoAComprar)){
+                productId = Integer.parseInt(id);
+            }
+        }
+        if (productId != -1) {
+            //Pilla el partner_id del res.users que lo solicita (para crear una venta se necesita en vez del user_id)
+            Map<String, Object> argsEncontrarPartnerId = new HashMap<>();
+            argsEncontrarPartnerId.put("service", "object");
+            argsEncontrarPartnerId.put("method", "execute");
+
+            argsEncontrarPartnerId.put("args", new Object[]{db, adminId, adminPassword, "res.users", "read",new Object[]{idUser},new String[]{"partner_id"}});
+
+            JsonRpcRequest requestEncontrarPartnerId = new JsonRpcRequest("call", argsEncontrarPartnerId, 2);
+            HttpEntity<JsonRpcRequest> entityEncontrarPartnerId = new HttpEntity<>(requestEncontrarPartnerId);
+
+            jsonResponse = Objects.requireNonNull(restTemplate.postForObject(url, entityEncontrarPartnerId, Object.class)).toString();
+
+            Pattern patternEncontrarPartnerId = Pattern.compile("partner_id=\\[([^\\]]+)]");
+            matcher = patternEncontrarPartnerId.matcher(jsonResponse);
+
+            int partnerId = -1;
+            if (matcher.find()) {
+                String[] numeros = matcher.group(1).split(",\\s*");
+                if (numeros.length > 0) {
+                    partnerId = Integer.parseInt(numeros[0]);
+                }
+            }
+            if(partnerId!=-1) {
+                //Crea la venta (vacia)
+                Map<String, Object> argsCrearVenta = new HashMap<>();
+                argsCrearVenta.put("service", "object");
+                argsCrearVenta.put("method", "execute");
+                //Se necesita copiar el id porque si no da error
+                int finalPartnerId = partnerId;
+                argsCrearVenta.put("args", new Object[]{db, adminId, adminPassword, "sale.order", "create", new Object[]{new HashMap<String, Object>() {{
+                    put("partner_id", finalPartnerId);
+                }}}});
+
+                JsonRpcRequest requestCrearVenta = new JsonRpcRequest("call", argsCrearVenta, 123456789);
+                HttpEntity<JsonRpcRequest> entityCrearVenta = new HttpEntity<>(requestCrearVenta);
+
+                jsonResponse = Objects.requireNonNull(restTemplate.postForObject(url, entityCrearVenta, Object.class)).toString();
+
+                Pattern idVentaPattern = Pattern.compile("\\[(\\d+)]");
+
+                matcher = idVentaPattern.matcher(jsonResponse);
+
+                int saleId = -1;
+
+                while (matcher.find()) {
+                    saleId = Integer.parseInt(matcher.group(1));
+                }
+
+                if (saleId != -1) {
+                    //Añade un producto a la venta (la Subscripcion Premium, cantidad = 1)
+                    Map<String, Object> argsProcesarVenta = new HashMap<>();
+                    argsProcesarVenta.put("service", "object");
+                    argsProcesarVenta.put("method", "execute");
+                    //Se necesita copiar el id de las cosas porque si no da error
+                    int finalProductId = productId;
+                    int finalSaleId = saleId;
+                    argsProcesarVenta.put("args", new Object[]{db, adminId, adminPassword, "sale.order.line", "create", new Object[]{new HashMap<String, Object>() {{
+                        put("order_id", finalSaleId);
+                        put("product_id", finalProductId);
+                        put("product_uom_qty", 1);
+                    }}}});
+
+                    JsonRpcRequest requestProcesarVenta = new JsonRpcRequest("call", argsProcesarVenta, 123456789);
+                    HttpEntity<JsonRpcRequest> entityProcesarVenta = new HttpEntity<>(requestProcesarVenta);
+
+                    restTemplate.postForObject(url, entityProcesarVenta, Object.class);
+
+                    Map<String, Object> argsConfirmarVenta = new HashMap<>();
+                    argsConfirmarVenta.put("service", "object");
+                    argsConfirmarVenta.put("method", "execute");
+                    argsConfirmarVenta.put("args", new Object[]{db, adminId, adminPassword, "sale.order", "action_confirm", new Object[]{finalSaleId}});
+
+                    JsonRpcRequest requestConfirmarVenta = new JsonRpcRequest("call", argsConfirmarVenta, 123456789);
+                    HttpEntity<JsonRpcRequest> entityConfirmarVenta = new HttpEntity<>(requestConfirmarVenta);
+
+                    //Como lo tenemos nosotros en Odoo al crear una venta automaticamente se aplica el Premium al Usuario
+                    return restTemplate.postForObject(url, entityConfirmarVenta, Object.class);
+                }
+            }
+            return null;
+        }
+        return null;
     }
 
 }
