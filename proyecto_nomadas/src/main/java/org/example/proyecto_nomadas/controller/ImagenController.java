c03b29fd11cfe4f41227e7f2bb2dd53a0322a936
Giorgio02K <jordichorrog96@gmail.com>
2024-02-19 16:21:47 +0100

    Añadidos parametros a ImagenController

--- proyecto_nomadas/src/main/java/org/example/proyecto_nomadas/controller/ImagenController.java
+++ proyecto_nomadas/src/main/java/org/example/proyecto_nomadas/controller/ImagenController.java
@@ -1,6 +1,7 @@
 package org.example.proyecto_nomadas.controller;
 
 import org.example.proyecto_nomadas.model.Imagen;
+import org.example.proyecto_nomadas.model.dto.ImagenDto;
 import org.example.proyecto_nomadas.repository.IImagenRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -8,7 +9,12 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/imagenes")
@@ -37,6 +43,57 @@ public class ImagenController {
         } else {
             return ResponseEntity.notFound().build();
         }
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<byte[]> getAll() {
+        List<Imagen> imagenes = repo.findAll();
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+        try {
+            for (Imagen imagen : imagenes) {
+                outputStream.write(imagen.getData());
+            }
+
+            byte[] allImageData = outputStream.toByteArray();
+
+            if (allImageData.length > 0) {
+                return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(allImageData);
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+
+
+
+
+
+
+    @GetMapping("/allId")
+    public List<Long> getAllId() {
+        List<Imagen> imagenes = repo.findAll();
+        List<Long> ids = new ArrayList<>();
+
+        for (Imagen imagen : imagenes) {
+            ids.add(imagen.getId());
+        }
+
+        return ids;
+    }
+
+
+    @DeleteMapping("/delete/all")
+    public void deleteAll() {
+        repo.deleteAll();
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteById(@PathVariable Long id) {
+        repo.deleteById(id);
     }
 }
 
